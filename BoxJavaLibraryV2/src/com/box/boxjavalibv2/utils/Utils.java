0c3cecd685442494bdaab55852a1303e31efa26f
Eric Dorman <edorman@box.com>
2014-02-25 18:55:42 -0800

    EntityUtils fixes
    EntityUtils.consumeQuietly is only available in HttpClient 4.1 and above, which breaks the
    android SDK, since android bundles an earlier version of HttpClient with it. We're moving
    these methods into the project.

--- BoxJavaLibraryV2/src/com/box/boxjavalibv2/utils/Utils.java
+++ BoxJavaLibraryV2/src/com/box/boxjavalibv2/utils/Utils.java
@@ -1,5 +1,10 @@
 package com.box.boxjavalibv2.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+
 import com.box.boxjavalibv2.dao.BoxResourceType;
 
 /**
@@ -26,6 +31,46 @@ public final class Utils {
                 return "versions";
             default:
                 return type.toPluralString();
+        }
+    }
+
+    /**
+     * Ensures that the entity content is fully consumed and the content stream, if exists, is closed. The process is done, <i>quietly</i> , without throwing
+     * any IOException.
+     * 
+     * @param entity
+     *            the entity to consume.
+     * 
+     * 
+     *            Copied from org.apache.http.util.EntityUtils
+     */
+    public static void consumeHttpEntityQuietly(final HttpEntity entity) {
+        try {
+            consumeHttpEntity(entity);
+        }
+        catch (final IOException ignore) {
+        }
+    }
+
+    /**
+     * Ensures that the entity content is fully consumed and the content stream, if exists, is closed.
+     * 
+     * @param entity
+     *            the entity to consume.
+     * @throws IOException
+     *             if an error occurs reading the input stream
+     * 
+     *             Copied from org.apache.http.util.EntityUtils
+     */
+    public static void consumeHttpEntity(final HttpEntity entity) throws IOException {
+        if (entity == null) {
+            return;
+        }
+        if (entity.isStreaming()) {
+            final InputStream instream = entity.getContent();
+            if (instream != null) {
+                instream.close();
+            }
         }
     }
 
