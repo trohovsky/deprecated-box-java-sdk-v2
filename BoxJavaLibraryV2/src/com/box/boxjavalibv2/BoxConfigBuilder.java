646a115e38709911aeb35626827a5ddf5ba5caa6
jian lin <jian@box.com>
2014-05-23 15:09:13 -0700

    update version number

--- BoxJavaLibraryV2/src/com/box/boxjavalibv2/BoxConfigBuilder.java
+++ BoxJavaLibraryV2/src/com/box/boxjavalibv2/BoxConfigBuilder.java
@@ -2,7 +2,7 @@ package com.box.boxjavalibv2;
 
 public class BoxConfigBuilder {
 
-    private final static String VERSION_NUMBER = "v3.0.6";
+    private final static String VERSION_NUMBER = "v3.0.7";
     /** Default API url scheme. */
     private static final String API_URL_SCHEME = "https";
     /** Default API url authority. */
