37d695834294bd915639d295855dd85ea361e66e
Mike Morearty <mmorearty@box.com>
2014-06-19 14:06:24 -0700

    When uploading files, use "parent_id" instead of "folder_id"
    When you upload a file, the documentation states that you should specify
    a field called "parent_id" which is the folder id of the folder into
    which the file should be uploaded.
    
    The API also accepts "folder_id"; but "parent_id" is the preferred name
    for the field. So, this commit changes the Java SDK to use "parent_id"
    instead of "folder_id".

--- BoxJavaLibraryV2/src/com/box/boxjavalibv2/utils/Constants.java
+++ BoxJavaLibraryV2/src/com/box/boxjavalibv2/utils/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String CONTENT_MD5 = "Content-MD5";
     public static final String RANGE = "Range";
 
-    public static final String FOLDER_ID = "folder_id";
+    public static final String PARENT_ID = "parent_id";
 
     public static final String ID = "id";
 
