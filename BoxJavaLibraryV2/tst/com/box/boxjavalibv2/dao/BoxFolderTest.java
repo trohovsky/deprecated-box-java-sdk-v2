8d5fc299f9bbe78a9ee48c338c5ebf61b93c40b7
dcung <dcung@box.com>
2014-07-16 14:55:21 -0700

    Update fields and test for BoxFolder.

--- BoxJavaLibraryV2/tst/com/box/boxjavalibv2/dao/BoxFolderTest.java
+++ BoxJavaLibraryV2/tst/com/box/boxjavalibv2/dao/BoxFolderTest.java
@@ -18,20 +18,84 @@ public class BoxFolderTest {
 
     @Test
     public void testParcelRoundTrip() throws BoxRestException, IOException, BoxJSONException {
-        String emailJson = FileUtils.readFileToString(new File("testdata/email.json"));
+
 
         String folderJson = FileUtils.readFileToString(new File("testdata/folder.json"));
-        folderJson = folderJson.replace("$folder_upload_email", emailJson);
         BoxFolder folder = (BoxFolder) TestUtils.getFromJSON(folderJson, BoxFolder.class);
 
         TestParcel parcel = new TestParcel();
         folder.writeToParcel(parcel, 0);
         BoxFolder fromParcel = new BoxFolder(parcel);
-        String uploadEmailString = (new BoxJSONParser(new BoxResourceHub()).convertBoxObjectToJSONString(fromParcel.getFolderUploadEmail()));
-        String[] parts = uploadEmailString.split(",");
-        Assert.assertEquals(2, parts.length);
-        Assert.assertTrue(emailJson.contains(parts[0].replace("{", "").replace("}", "")));
-        Assert.assertTrue(emailJson.contains(parts[1].replace("{", "").replace("}", "")));
+
+
+        Assert.assertEquals("folder", fromParcel.getType());
+        Assert.assertEquals("11446498", fromParcel.getId());
+        Assert.assertEquals("1", fromParcel.getSequenceId());
+        Assert.assertEquals("1", fromParcel.getEtag());
+        Assert.assertEquals("Pictures", fromParcel.getName());
+        Assert.assertEquals("2012-12-12T10:53:43-08:00", fromParcel.getCreatedAt());
+        Assert.assertEquals("2012-12-12T11:15:04-08:00", fromParcel.getModifiedAt());
+        Assert.assertEquals("Some pictures I took", fromParcel.getDescription());
+        Assert.assertEquals(629644, fromParcel.getSize().doubleValue(), 0);
+
+        Assert.assertEquals(1, fromParcel.getPathCollection().getTotalCount().intValue());
+        Assert.assertEquals("folder",fromParcel.getPathCollection().getEntries().get(0).getType());
+        Assert.assertEquals("0",fromParcel.getPathCollection().getEntries().get(0).getId());
+        Assert.assertEquals(null,((BoxItem)fromParcel.getPathCollection().getEntries().get(0)).getSequenceId());
+        Assert.assertEquals(null,((BoxItem) fromParcel.getPathCollection().getEntries().get(0)).getEtag());
+        Assert.assertEquals("All Files", ((BoxItem) fromParcel.getPathCollection().getEntries().get(0)).getName());
+
+        Assert.assertEquals("user",fromParcel.getCreatedBy().getType());
+        Assert.assertEquals("17738362",fromParcel.getCreatedBy().getId());
+        Assert.assertEquals("sean rose",fromParcel.getCreatedBy().getName());
+        Assert.assertEquals("sean@box.com",fromParcel.getCreatedBy().getLogin());
+
+        Assert.assertEquals("user",fromParcel.getModifiedBy().getType());
+        Assert.assertEquals("17738362",fromParcel.getModifiedBy().getId());
+        Assert.assertEquals("sean rose",fromParcel.getModifiedBy().getName());
+        Assert.assertEquals("sean@box.com",fromParcel.getModifiedBy().getLogin());
+
+        Assert.assertEquals("user",fromParcel.getOwnedBy().getType());
+        Assert.assertEquals("17738362",fromParcel.getOwnedBy().getId());
+        Assert.assertEquals("sean rose",fromParcel.getOwnedBy().getName());
+        Assert.assertEquals("sean@box.com",fromParcel.getOwnedBy().getLogin());
+
+        Assert.assertEquals("https://www.box.com/s/vspke7y05sb214wjokpk",fromParcel.getSharedLink().getUrl());
+        Assert.assertEquals(null,fromParcel.getSharedLink().getDownloadUrl());
+        Assert.assertEquals(null,fromParcel.getSharedLink().getVanityUrl());
+        Assert.assertEquals(false,fromParcel.getSharedLink().isPasswordEnabled());
+        Assert.assertEquals(null,fromParcel.getSharedLink().getUnsharedAt());
+        Assert.assertEquals(0,fromParcel.getSharedLink().getDownloadCount().intValue());
+        Assert.assertEquals(0,fromParcel.getSharedLink().getPreviewCount().intValue());
+        Assert.assertEquals("open",fromParcel.getSharedLink().getAccess());
+        Assert.assertEquals(true,fromParcel.getSharedLink().getPermissions().isCan_download());
+        Assert.assertEquals(true,fromParcel.getSharedLink().getPermissions().canPreview());
+
+        Assert.assertEquals("open", fromParcel.getFolderUploadEmail().getAccess());
+        Assert.assertEquals("upload.Picture.k13sdz1@u.box.com", fromParcel.getFolderUploadEmail().getEmail());
+
+        Assert.assertEquals("folder", fromParcel.getParent().getType());
+        Assert.assertEquals("0", fromParcel.getParent().getId());
+        Assert.assertEquals(null, fromParcel.getParent().getSequenceId());
+        Assert.assertEquals(null, fromParcel.getParent().getEtag());
+        Assert.assertEquals("All Files", fromParcel.getParent().getName());
+
+
+        Assert.assertEquals("active", fromParcel.getItemStatus());
+
+        Assert.assertEquals(1, fromParcel.getItemCollection().getTotalCount().intValue());
+        Assert.assertEquals("file", fromParcel.getItemCollection().getEntries().get(0).getType());
+        Assert.assertEquals("5000948880", fromParcel.getItemCollection().getEntries().get(0).getId());
+        Assert.assertEquals("3", ((BoxItem)fromParcel.getItemCollection().getEntries().get(0)).getSequenceId());
+        Assert.assertEquals("3", ((BoxItem) fromParcel.getItemCollection().getEntries().get(0)).getEtag());
+        Assert.assertEquals("134b65991ed521fcfe4724b7d814ab8ded5185dc", ((BoxFile)fromParcel.getItemCollection().getEntries().get(0)).getSha1());
+        Assert.assertEquals("tigers.jpeg", ((BoxItem) fromParcel.getItemCollection().getEntries().get(0)).getName());
+
+        Assert.assertEquals(2,fromParcel.getTags().length);
+        Assert.assertEquals("approved",fromParcel.getTags()[0]);
+        Assert.assertEquals("ready to publish",fromParcel.getTags()[1]);
+
         Assert.assertEquals(false, (boolean) fromParcel.hasCollaborations());
+        Assert.assertEquals("not_synced", fromParcel.getSyncState());
     }
 }
